de8042f301cf043487a915c8a211e3755f9960bb
Harshit <vgera2001@gmail.com>
2023-07-26 18:30:14 +0530

    1.02

---
+++ client/src/components/Tipcard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Tipcard = () => {
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+
+    )
+}
+
+export default Tipcard
--- client/src/components/Tipgrid.jsx
+++ client/src/components/Tipgrid.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Tipgrid = () => {
   return (
     <>
-    <div style={{height:"400px",background:"rgb(225, 225, 225)"}}>
-
-    </div>
+      <div style={{ height: "400px", background: "rgb(225, 225, 225)" }}>
+        <div style={{ marginLeft: "130px", paddingTop: "20px", fontSize: "24px", fontWeight: "700" }}>Photography Tips</div>
+      </div>
     </>
   )
 }
